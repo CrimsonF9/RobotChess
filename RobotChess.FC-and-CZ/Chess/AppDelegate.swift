dbb4af5869866742cd1258959341bef8e67443dc
CrimsonF9 <111999733+CrimsonF9@users.noreply.github.com>
2023-04-27 13:56:02 -0500

    Add files via upload

---
+++ RobotChess.FC-and-CZ/Chess/AppDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  AppDelegate.swift
+//  RobotChess.FC-and-CZ
+//
+//  Created by Fernando Castro on 4/15/23.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {}
---
+++ RobotChess.FC-and-CZ/Chess/SceneDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  SceneDelegate.swift
+//  RobotChess.FC-and-CZ
+//
+//  Created by Fernando Castro on 4/15/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+}
